d878ffcce3e72c6c8bdc9e0a9a3d4bbc8985f68a
SpaceOne <space@wechall.net>
2015-06-20 02:57:44 +0200

    Initial commit

---
+++ httoop/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""HTTP/2 components for httoop"""
+
+try:
+	__import__('pkg_resources').declare_namespace(__name__)
+except ImportError:  # pragma: no cover
+	__path___ = __import__('pkgutil').extend_path(__path__, __name__)
